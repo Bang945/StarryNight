481f775185dd101fccd67294e96f7c373d709517
bang <bang@ubuntu.(none)>
2015-03-14 14:08:30 +0800

    add a diary Live in sunshine in db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,25 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Add seed data of Favor for testing.
+=begin
     Favor.create(content: "Jog", label: "sport")
     Favor.create(content: "One Piece", label: "entertainment") 
+=end
 
 # Add seed data of Contact for testing.
+=begin
     Contact.create(content: "Douban", label: "social", link: "http://www.douban.com")
     Contact.create(content: "Email:Banghui.luo@gmail.com", label: "email")
+=end
+
 
 # Add seed data of Diary for testing.
+	Diary.create(title: "Live in Sunshine", create_date: "2015.01.08", name: "live_in_sunshine.txt", label: 1)
+=begin
+
     Diary.create(title: "Live in Sunshine", text: "Sunshine", label:"wundering")
     Diary.create(title: "Smile under Blue Sky", text: "Smiling", label:"hanging")
     Diary.create(title: "Fly at Starry Night", text: "Starry", label:"wundering")
+=end
+
+
